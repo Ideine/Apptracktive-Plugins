727a9cecb2017fb22b57557f62a4f7a13c5b3d4a
Jérémy BRUN-PICARD <jeremy.brunpicard@apptracktive.com>
2015-11-08 09:05:33 +0100

    Uwp sample renamed

--- Aptk.Plugins.AzureMobileServices/Samples/Aptk.Plugins.AzureMobileServices.Sample.Uwp/Properties/AssemblyInfo.cs
+++ Aptk.Plugins.AzureMobileServices/Samples/Aptk.Plugins.AzureMobileServices.Sample.Uwp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aptk.Plugins.AzureMobileServices.Sample.Windows")]
+[assembly: AssemblyTitle("Aptk.Plugins.AzureMobileServices.Sample.Uwp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Aptk.Plugins.AzureMobileServices.Sample.Windows")]
+[assembly: AssemblyProduct("Aptk.Plugins.AzureMobileServices.Sample.Uwp")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

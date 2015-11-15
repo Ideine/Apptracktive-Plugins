8b6b3c9e2ab81e1e587d56a3bb8b18fe653f5fe9
Jérémy BRUN-PICARD <jeremy.brunpicard@apptracktive.com>
2015-11-15 13:03:46 +0100

    Configuration updated

--- AptkAms/Samples/AptkAms.Sample.Core/Constants.cs
+++ AptkAms/Samples/AptkAms.Sample.Core/Constants.cs
@@ -5,9 +5,9 @@ namespace AptkAms.Sample.Core
 {
     public static class Constants
     {
-        public static readonly string AmsAppUrl = "https://aptkamssamplebackend.azure-mobile.net/";
+        public static readonly string AmsAppUrl = "YOUR URL HERE";
 
-        public static readonly string AmsAppKey = "WweOcXBjVwkHbQYidZdkaJELdySxDy82";
+        public static readonly string AmsAppKey = "YOUR KEY HERE";
 
         public static readonly Assembly ModelAssembly = typeof(TodoItem).GetTypeInfo().Assembly;
     }
